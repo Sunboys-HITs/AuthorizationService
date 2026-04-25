b3b0cc03ac36806d3c073b5d638bad17cee74b8b
aspakaramych <vgrig2006@gmail.com>
2026-04-25 10:46:27 +0700

    init proj

---
+++ AuthorizationService/AuthorizationService.AppHost/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthorizationService.AppHost.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly ILogger<AuthController> _logger;
+
+    public AuthController(IMediator mediator, ILogger<AuthController> logger)
+    {
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+    
+    
+}
